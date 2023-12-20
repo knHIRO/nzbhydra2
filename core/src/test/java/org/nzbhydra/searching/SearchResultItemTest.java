2bdd0c81cc7cd828c5d8f782a10e6e59470ff3a6
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:41:00 +0900

    update file

---
+++ core/src/test/java/org/nzbhydra/searching/SearchResultItemTest.java
@@ -0,0 +1,33 @@
+package org.nzbhydra.searching;
+
+import org.junit.jupiter.api.Test;
+import org.nzbhydra.searching.dtoseventsenums.SearchResultItem;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SearchResultItemTest {
+
+    @Test
+    void compareTo() throws Exception {
+        Comparator<SearchResultItem> comparator = SearchResultItem.comparator();
+        SearchResultItem item1 = new SearchResultItem();
+        item1.setPubDate(Instant.now());
+        SearchResultItem item2 = new SearchResultItem();
+        assertThat(comparator.compare(item1, item2)).isEqualTo(1);
+        assertThat(comparator.compare(item2, item1)).isEqualTo(-1);
+        item1.setPubDate(null);
+        assertThat(comparator.compare(item2, item1)).isEqualTo(0);
+
+        item1.setPubDate(Instant.now());
+        item2.setPubDate(item1.getPubDate());
+        assertThat(comparator.compare(item2, item1)).isEqualTo(0);
+
+        item1.setPubDate(Instant.now().minus(1, ChronoUnit.DAYS));
+        assertThat(comparator.compare(item1, item2)).isEqualTo(-1);
+    }
+
+}
