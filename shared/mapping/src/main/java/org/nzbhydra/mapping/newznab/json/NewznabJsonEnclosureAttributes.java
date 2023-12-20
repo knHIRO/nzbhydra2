11d08ad9b713fe0b0db9b3a56dd4d77385cc4562
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:46:04 +0900

    update file

---
+++ shared/mapping/src/main/java/org/nzbhydra/mapping/newznab/json/NewznabJsonEnclosureAttributes.java
@@ -0,0 +1,37 @@
+
+package org.nzbhydra.mapping.newznab.json;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.nzbhydra.springnative.ReflectionMarker;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "url",
+    "length",
+    "type"
+})
+@Data
+@ReflectionMarker
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewznabJsonEnclosureAttributes {
+
+    public NewznabJsonEnclosureAttributes(String url, long length, String type) {
+        this.url = url;
+        this.length = String.valueOf(length);
+        this.type = type;
+    }
+
+    @JsonProperty("url")
+    public String url;
+    @JsonProperty("length")
+    public String length;
+    @JsonProperty("type")
+    public String type;
+
+}
