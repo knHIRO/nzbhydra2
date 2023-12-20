2e43f8e0d098b26f5d70bc08108181e637ee7b85
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:42:15 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/config/safeconfig/SafeDownloaderConfig.java
@@ -0,0 +1,28 @@
+package org.nzbhydra.config.safeconfig;
+
+import lombok.Data;
+import org.nzbhydra.config.downloading.DownloaderConfig;
+import org.nzbhydra.springnative.ReflectionMarker;
+
+@Data
+@ReflectionMarker
+public class SafeDownloaderConfig {
+
+    private String defaultCategory;
+    private String downloadType;
+    private boolean enabled = true;
+    private String iconCssClass;
+    private String name;
+    private String downloaderType;
+
+    public SafeDownloaderConfig(DownloaderConfig downloaderConfig) {
+        this.defaultCategory = downloaderConfig.getDefaultCategory();
+        this.downloadType = downloaderConfig.getDownloadType().name();
+        this.enabled = downloaderConfig.isEnabled();
+        this.iconCssClass = downloaderConfig.getIconCssClass();
+        this.name = downloaderConfig.getName();
+        this.downloaderType = downloaderConfig.getDownloaderType().name();
+    }
+
+
+}
