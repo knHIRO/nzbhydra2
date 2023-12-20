f9544a5be43f3f5c2c1f554095a42945c5076c77
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:39:47 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/indexers/IndexerEntity.java
@@ -0,0 +1,62 @@
+package org.nzbhydra.indexers;
+
+import com.google.common.base.MoreObjects;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.nzbhydra.springnative.ReflectionMarker;
+
+import java.util.Objects;
+
+
+@Data
+@ReflectionMarker
+@Entity
+@Table(name = "indexer")
+public final class IndexerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(allocationSize = 1, name = "INDEXER_SEQ")
+    private int id;
+
+    @Column(unique = true)
+    private String name;
+
+    public IndexerEntity() {
+    }
+
+    public IndexerEntity(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        IndexerEntity that = (IndexerEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("name", name)
+                .toString();
+    }
+}
