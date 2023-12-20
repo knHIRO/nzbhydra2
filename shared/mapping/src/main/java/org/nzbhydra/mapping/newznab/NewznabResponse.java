9e1517a8af8a08d4af6a016ec5d541ae1a4566d1
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:45:30 +0900

    update file

---
+++ shared/mapping/src/main/java/org/nzbhydra/mapping/newznab/NewznabResponse.java
@@ -0,0 +1,49 @@
+/*
+ *  (C) Copyright 2017 TheOtherP (theotherp@posteo.net)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.nzbhydra.mapping.newznab;
+
+
+import jakarta.xml.bind.JAXB;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+public abstract class NewznabResponse {
+
+    public enum SearchType {
+        JSON,
+        NEWZNAB,
+        TORZNAB
+
+    }
+
+    public abstract String getContentHeader();
+
+    public abstract SearchType getSearchType();
+
+    public abstract void setSearchType(NewznabResponse.SearchType searchType);
+
+    public String toXmlString() {
+        try (StringWriter writer = new StringWriter()) {
+            JAXB.marshal(this, writer);
+            return writer.toString();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+}
