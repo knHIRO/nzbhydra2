5315f9fde1a90f56f6dce18f4d5a6a885e7d8035
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:42:35 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/indexers/status/IndexerLimit.java
@@ -0,0 +1,44 @@
+package org.nzbhydra.indexers.status;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.nzbhydra.indexers.IndexerEntity;
+import org.nzbhydra.springnative.ReflectionMarker;
+
+import java.time.Instant;
+
+@Data
+@ReflectionMarker
+@Entity
+@Table(name = "indexerlimit")
+
+public final class IndexerLimit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(allocationSize = 1, name = "INDEXERLIMIT_SEQ")
+    protected int id;
+
+    @OneToOne
+    private IndexerEntity indexer;
+    private Integer apiHits;
+    private Integer apiHitLimit;
+    private Integer downloads;
+    private Integer downloadLimit;
+    private Instant oldestApiHit;
+    private Instant oldestDownload;
+
+
+    public IndexerLimit(IndexerEntity indexer) {
+        this.indexer = indexer;
+    }
+
+    public IndexerLimit() {
+    }
+}
