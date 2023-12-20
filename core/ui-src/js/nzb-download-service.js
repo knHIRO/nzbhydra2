2a0f7a070205ddbf805116ae4d0b94d47b5189e1
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:05 +0900

    update file

---
+++ core/ui-src/js/nzb-download-service.js
@@ -0,0 +1,40 @@
+angular
+    .module('nzbhydraApp')
+    .factory('NzbDownloadService', NzbDownloadService);
+
+function NzbDownloadService($http, ConfigService, DownloaderCategoriesService) {
+
+    var service = {
+        download: download,
+        getEnabledDownloaders: getEnabledDownloaders
+    };
+
+    return service;
+
+    function sendNzbAddCommand(downloader, searchResults, category) {
+        var params = {
+            downloaderName: downloader.name,
+            searchResults: searchResults,
+            category: category
+        };
+        return $http.put("internalapi/downloader/addNzbs", params);
+    }
+
+    function download(downloader, searchResults, alwaysAsk) {
+        var category = downloader.defaultCategory;
+        if (alwaysAsk || (_.isNullOrEmpty(category) && category !== "Use original category") && category !== "Use mapped category" && category !== "Use no category") {
+            return DownloaderCategoriesService.openCategorySelection(downloader).then(function (category) {
+                return sendNzbAddCommand(downloader, searchResults, category);
+            }, function (result) {
+                return result;
+            });
+        } else {
+            return sendNzbAddCommand(downloader, searchResults, category)
+        }
+    }
+
+    function getEnabledDownloaders() {
+        return _.filter(ConfigService.getSafe().downloading.downloaders, "enabled");
+    }
+}
+
