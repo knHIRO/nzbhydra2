9615edbf372eff4160687fdcb78f9bbf4e11acf4
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:42:49 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/searching/dtoseventsenums/SearchRequestCacheKey.java
@@ -0,0 +1,72 @@
+/*
+ *  (C) Copyright 2017 TheOtherP (theotherp@posteo.net)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.nzbhydra.searching.dtoseventsenums;
+
+import com.google.common.base.Objects;
+import org.nzbhydra.searching.searchrequests.SearchRequest;
+
+public class SearchRequestCacheKey {
+
+    private final SearchRequest searchRequest;
+
+    public SearchRequestCacheKey(SearchRequest searchRequest) {
+        this.searchRequest = searchRequest;
+    }
+
+    public SearchRequest getSearchRequest() {
+        return searchRequest;
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final SearchRequestCacheKey other = (SearchRequestCacheKey) obj;
+        return
+                Objects.equal(searchRequest.getQuery(), other.getSearchRequest().getQuery())
+                        && Objects.equal(searchRequest.getSeason(), other.getSearchRequest().getSeason())
+                        && Objects.equal(searchRequest.getEpisode(), other.getSearchRequest().getEpisode())
+                        && Objects.equal(searchRequest.getIdentifiers(), other.getSearchRequest().getIdentifiers())
+                        && Objects.equal(searchRequest.getAuthor(), other.getSearchRequest().getAuthor())
+                        && Objects.equal(searchRequest.getTitle(), other.getSearchRequest().getTitle())
+                        && Objects.equal(searchRequest.getMinage(), other.getSearchRequest().getMinage())
+                        && Objects.equal(searchRequest.getMaxage(), other.getSearchRequest().getMaxage())
+                        && Objects.equal(searchRequest.getMinsize(), other.getSearchRequest().getMinsize())
+                        && Objects.equal(searchRequest.getMaxsize(), other.getSearchRequest().getMaxsize())
+                ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(
+                searchRequest.getQuery(),
+                searchRequest.getSeason(),
+                searchRequest.getEpisode(),
+                searchRequest.getIdentifiers(),
+                searchRequest.getAuthor(),
+                searchRequest.getTitle(),
+                searchRequest.getMinage(),
+                searchRequest.getMaxage(),
+                searchRequest.getMinsize(),
+                searchRequest.getMaxsize()
+        );
+    }
+}
