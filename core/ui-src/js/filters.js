ddf25e6a6e23bc3a0b2871cf1191598bf8a6a3e1
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:00 +0900

    update file

---
+++ core/ui-src/js/filters.js
@@ -0,0 +1,15 @@
+var filters = angular.module('filters', []);
+
+filters.filter('bytes', function () {
+    return function (bytes) {
+        return filesize(bytes);
+    }
+});
+
+filters
+    .filter('unsafe', ['$sce', function ($sce) {
+        return function (text) {
+            return $sce.trustAsHtml(text);
+        };
+    }]);
+
