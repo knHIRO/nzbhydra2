5ab06a3469cde16465b589c313ea38a43ca072a0
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:40:05 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/misc/UserAgentMapper.java
@@ -0,0 +1,33 @@
+package org.nzbhydra.misc;
+
+import org.nzbhydra.config.ConfigProvider;
+import org.nzbhydra.logging.LoggingMarkers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserAgentMapper {
+
+    @Autowired
+    private ConfigProvider configProvider;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserAgentMapper.class);
+
+    public String getUserAgent(String userAgent) {
+        if (userAgent == null) {
+            logger.debug(LoggingMarkers.USER_AGENT, "No user agent provided");
+            return null;
+        }
+        String headerLowercase = userAgent.toLowerCase();
+        for (String toCompare : configProvider.getBaseConfig().getSearching().getUserAgents()) {
+            if (headerLowercase.contains(toCompare.toLowerCase())) {
+                logger.debug(LoggingMarkers.USER_AGENT, "User agent '{} mapped to '{}'", userAgent, toCompare);
+                return toCompare;
+            }
+        }
+        logger.debug(LoggingMarkers.USER_AGENT, "Unknown user agent '{}'", userAgent);
+        return "Other";
+    }
+}
