c3736533a4f81f86427830f57734671f9ec080fe
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:40:07 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/news/ShownNewsRepository.java
@@ -0,0 +1,9 @@
+package org.nzbhydra.news;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShownNewsRepository extends JpaRepository<ShownNews, Integer> {
+
+
+}
