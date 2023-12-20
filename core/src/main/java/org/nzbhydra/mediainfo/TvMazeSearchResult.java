49f4dc66311ee1897cb97f6ed6b990b8b3f8c9ce
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:40:03 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/mediainfo/TvMazeSearchResult.java
@@ -0,0 +1,35 @@
+package org.nzbhydra.mediainfo;
+
+import com.google.common.base.MoreObjects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.nzbhydra.springnative.ReflectionMarker;
+
+@Data
+@ReflectionMarker
+@AllArgsConstructor
+@NoArgsConstructor
+class TvMazeSearchResult {
+
+    private String tvmazeId;
+    private String tvrageId;
+    private String tvdbId;
+    private String imdbId;
+    private String title;
+    private Integer year;
+    private String posterUrl;
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("tvmazeId", tvmazeId)
+                .add("tvrageId", tvrageId)
+                .add("tvdbId", tvdbId)
+                .add("imdbId", imdbId)
+                .add("title", title)
+                .add("year", year)
+                .add("posterUrl", posterUrl)
+                .toString();
+    }
+}
