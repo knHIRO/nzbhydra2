fbd4fb25d4de764af81458d8a10cef3f508689c9
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:40:50 +0900

    update file

---
+++ core/src/test/java/org/nzbhydra/indexers/NzbGeekTest.java
@@ -0,0 +1,21 @@
+package org.nzbhydra.indexers;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NzbGeekTest {
+
+    @InjectMocks
+    private NzbGeek testee = new NzbGeek(null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+
+
+    @Test
+    void shouldNotUseMoreThan6WordsForNzbGeek() throws Exception {
+        String query = "1 2 3 4 5 6 7 8 9";
+        assertThat(testee.cleanupQuery(query)).isEqualTo("1 2 3 4 5 6");
+    }
+
+
+}
