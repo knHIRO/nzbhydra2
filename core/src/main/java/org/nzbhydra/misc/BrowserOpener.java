306ab177ca299cf6663406fc5d42a4d801a1bd21
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:40:03 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/misc/BrowserOpener.java
@@ -0,0 +1,41 @@
+package org.nzbhydra.misc;
+
+import org.nzbhydra.web.UrlCalculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.net.URI;
+
+@Component
+public class BrowserOpener {
+
+    private static final Logger logger = LoggerFactory.getLogger(BrowserOpener.class);
+
+    @Autowired
+    private UrlCalculator urlCalculator;
+
+    public void openBrowser() {
+        Desktop desktop;
+        try {
+            desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+        } catch (Throwable e) {
+            logger.debug("Unable to get desktop");
+            return;
+        }
+        URI uri = urlCalculator.getLocalBaseUriBuilder().build().toUri();
+        if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+            logger.info("Opening {} in browser", uri);
+            try {
+                desktop.browse(uri);
+            } catch (Exception e) {
+                logger.error("Unable to open browser. Go to " + uri.toString(), e);
+            }
+        } else {
+            logger.error("Unable to open browser. Go to " + uri.toString());
+        }
+    }
+
+}
