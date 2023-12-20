7b5d9baf929ef9ea80ef486d2d32d9524060f5e2
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:40:19 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/searching/SearchModuleConfigProvider.java
@@ -0,0 +1,49 @@
+package org.nzbhydra.searching;
+
+import org.nzbhydra.config.ConfigChangedEvent;
+import org.nzbhydra.config.ConfigProvider;
+import org.nzbhydra.config.indexer.IndexerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ConfigurationProperties
+@EnableConfigurationProperties
+public class SearchModuleConfigProvider implements InitializingBean {
+
+    private static final Logger logger = LoggerFactory.getLogger(SearchModuleConfigProvider.class);
+
+    private List<IndexerConfig> indexers;
+    @Autowired
+    private SearchModuleProvider searchModuleProvider;
+    @Autowired
+    private ConfigProvider configProvider;
+
+    @EventListener
+    public void handleNewConfig(ConfigChangedEvent configChangedEvent) {
+        afterPropertiesSet();
+    }
+
+    public void setIndexers(List<IndexerConfig> indexers) {
+        this.indexers = indexers;
+    }
+
+
+    public List<IndexerConfig> getIndexers() {
+        return indexers;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        indexers = configProvider.getBaseConfig().getIndexers();
+        searchModuleProvider.loadIndexers(indexers);
+    }
+}
