b2975ada6dbb192ed4d391f58ed6a6d132c8c86a
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:40:23 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/springconfig/AppConfig.java
@@ -0,0 +1,27 @@
+package org.nzbhydra.springconfig;
+
+import org.nzbhydra.webaccess.HydraOkHttp3ClientHttpRequestFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
+
+
+@EnableAsync
+@Configuration(proxyBeanMethods = false)
+public class AppConfig {
+
+    @Autowired
+    private HydraOkHttp3ClientHttpRequestFactory requestFactory;
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        HydraOkHttp3ClientHttpRequestFactory factory = requestFactory;
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(factory);
+        return restTemplate;
+    }
+
+}
