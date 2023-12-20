e3f6790ed4bfae8dbdc1016eff3ece28bf9ae75a
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:46:49 +0900

    update file

---
+++ shared/mapping/src/main/java/org/nzbhydra/mapping/newznab/json/caps/CapsJsonSearching.java
@@ -0,0 +1,36 @@
+
+package org.nzbhydra.mapping.newznab.json.caps;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.nzbhydra.springnative.ReflectionMarker;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "search",
+        "tv-search",
+        "movie-search",
+        "audio-search"
+})
+@Data
+@ReflectionMarker
+@AllArgsConstructor
+@NoArgsConstructor
+public class CapsJsonSearching {
+
+    @JsonProperty("search")
+    private CapsJsonSearchIdAttributesHolder search;
+    @JsonProperty("tv-search")
+    private CapsJsonSearchIdAttributesHolder tvSearch;
+    @JsonProperty("movie-search")
+    private CapsJsonSearchIdAttributesHolder movieSearch;
+    @JsonProperty("audio-search")
+    private CapsJsonSearchIdAttributesHolder audioSearch;
+    @JsonProperty("book-search")
+    private CapsJsonSearchIdAttributesHolder bookSearch;
+
+}
