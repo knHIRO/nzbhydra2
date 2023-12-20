afc8c1fed870eabcd55f7dbc80ff2f6846104bea
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:43:02 +0900

    update file

---
+++ other/mockserver/src/main/java/org/nzbhydra/mockserver/WebConfiguration.java
@@ -0,0 +1,80 @@
+package org.nzbhydra.mockserver;
+
+import jakarta.xml.bind.Marshaller;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+            .addResourceLocations("classpath:/static/")
+            .setCacheControl(CacheControl.noCache())
+            .resourceChain(false)
+        //.addResolver(new VersionResourceResolver().addContentVersionStrategy("/static/js/**", "/static/css/**"))
+        ;
+
+
+        registry.setOrder(0);
+    }
+
+//    @Bean
+//    public RequestMappingHandlerMapping requestMappingHandlerMapping() {
+//        RequestMappingHandlerMapping handler = super.requestMappingHandlerMapping();
+//        handler.setOrder(1);
+//        return handler;
+//    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setForceEncoding(true);
+        characterEncodingFilter.setEncoding("UTF-8");
+        registrationBean.setFilter(characterEncodingFilter);
+        return registrationBean;
+    }
+
+    /**
+     * Enable pretty printing of returned XML
+     */
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        Map<String, Boolean> map = new HashMap<>();
+        map.put(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        marshaller.setMarshallerProperties(map);
+        marshaller.setPackagesToScan("org.nzbhydra.mockserver");
+        return marshaller;
+    }
+
+    /**
+     * Enable pretty printing of returned JSON
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        for (HttpMessageConverter<?> converter : converters) {
+            if (converter instanceof MappingJackson2HttpMessageConverter) {
+                MappingJackson2HttpMessageConverter jacksonConverter = (MappingJackson2HttpMessageConverter) converter;
+                jacksonConverter.setPrettyPrint(true);
+            }
+        }
+    }
+
+
+}
