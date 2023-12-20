098f3f41e818056ddddf57b93f24512f6e672dea
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:39:45 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/indexers/IndexerApiAccessEntityShort.java
@@ -0,0 +1,56 @@
+package org.nzbhydra.indexers;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.nzbhydra.springnative.ReflectionMarker;
+
+import java.time.Instant;
+
+
+/**
+ * Stores indexer API accesses that will only be kept in the database for no more than 24 hours. Makes
+ * checking for API hit limits much faster
+ */
+@Data
+@ReflectionMarker
+@Entity
+@NoArgsConstructor
+@Table(name = "indexerapiaccess_short")
+public final class IndexerApiAccessEntityShort {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(allocationSize = 1, name = "INDEXERAPIACCESS_SHORT_SEQ")
+    protected int id;
+
+    @Column(name = "INDEXER_ID")
+    private int indexerId;
+
+    @Convert(converter = org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters.InstantConverter.class)
+    private Instant time;
+
+    private boolean successful;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "API_ACCESS_TYPE")
+    private IndexerApiAccessType apiAccessType;
+
+    public IndexerApiAccessEntityShort(IndexerEntity indexerEntity, boolean successsful, IndexerApiAccessType apiAccessType) {
+        this.indexerId = indexerEntity.getId();
+        time = Instant.now();
+        this.successful = successsful;
+        this.apiAccessType = apiAccessType;
+    }
+
+
+}
