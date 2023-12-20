4106e7720e923f03f9397ac28f550fd2b149b22d
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:46:47 +0900

    update file

---
+++ shared/mapping/src/main/java/org/nzbhydra/mapping/newznab/json/caps/CapsJsonLimits.java
@@ -0,0 +1,25 @@
+
+package org.nzbhydra.mapping.newznab.json.caps;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.nzbhydra.springnative.ReflectionMarker;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "@attributes"
+})
+@Data
+@ReflectionMarker
+@AllArgsConstructor
+@NoArgsConstructor
+public class CapsJsonLimits {
+
+    @JsonProperty("@attributes")
+    private CapsJsonLimitsAttributes attributes;
+
+}
