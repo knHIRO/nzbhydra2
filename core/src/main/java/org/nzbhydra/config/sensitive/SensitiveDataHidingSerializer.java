c7c9a114a186b633a5a33a51d435becc77408c67
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:42:17 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/config/sensitive/SensitiveDataHidingSerializer.java
@@ -0,0 +1,25 @@
+package org.nzbhydra.config.sensitive;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class SensitiveDataHidingSerializer extends JsonSerializer<Object> {
+
+    private static final Logger logger = LoggerFactory.getLogger(SensitiveDataHidingSerializer.class);
+
+    @Override
+    public void serialize(Object value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        String toWrite = "<REMOVED>";
+        if (value instanceof Optional optional) {
+            toWrite = optional.isPresent() ? "<REMOVED>" : "<NOTSET>";
+        }
+        logger.debug("Hiding sensitive data in config setting \"{}\"", gen.getOutputContext().getCurrentName());
+        gen.writeString(toWrite);
+    }
+}
