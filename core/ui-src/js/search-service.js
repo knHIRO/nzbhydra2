15306cb402296af6ed86fcc39b345bbeb99aebe4
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:08 +0900

    update file

---
+++ core/ui-src/js/search-service.js
@@ -0,0 +1,104 @@
+//
+angular
+    .module('nzbhydraApp')
+    .factory('SearchService', SearchService);
+
+function SearchService($http) {
+
+
+    var lastExecutedQuery;
+    var lastExecutedSearchRequestParameters;
+    var lastResults;
+    var modalInstance;
+
+    return {
+        search: search,
+        getLastResults: getLastResults,
+        loadMore: loadMore,
+        getModalInstance: getModalInstance,
+        setModalInstance: setModalInstance,
+    };
+
+    function getModalInstance() {
+        return modalInstance;
+    }
+
+    function setModalInstance(mi) {
+        modalInstance = mi;
+    }
+
+    function search(searchRequestId, category, query, metaData, season, episode, minsize, maxsize, minage, maxage, indexers, mode) {
+        // console.time("search");
+        var uri = new URI("internalapi/search");
+        var searchRequestParameters = {};
+        searchRequestParameters.searchRequestId = searchRequestId;
+        searchRequestParameters.query = query;
+        searchRequestParameters.minsize = minsize;
+        searchRequestParameters.maxsize = maxsize;
+        searchRequestParameters.minage = minage;
+        searchRequestParameters.maxage = maxage;
+        searchRequestParameters.category = category;
+        searchRequestParameters.mode = mode;
+        if (!angular.isUndefined(indexers) && indexers !== null) {
+            searchRequestParameters.indexers = indexers.split(",");
+        }
+
+        if (metaData) {
+            searchRequestParameters.title = metaData.title;
+            if (category.indexOf("Movies") > -1 || (category.indexOf("20") === 0) || mode === "movie") {
+                searchRequestParameters.tmdbId = metaData.tmdbId;
+                searchRequestParameters.imdbId = metaData.imdbId;
+            } else if (category.indexOf("TV") > -1 || (category.indexOf("50") === 0) || mode === "tvsearch") {
+                searchRequestParameters.tvdbId = metaData.tvdbId;
+                searchRequestParameters.tvrageId = metaData.rid;
+                searchRequestParameters.tvmazeId = metaData.tvmazeId;
+                searchRequestParameters.season = season;
+                searchRequestParameters.episode = episode;
+            }
+        }
+
+        lastExecutedQuery = uri;
+        lastExecutedSearchRequestParameters = searchRequestParameters;
+        return $http.post(uri.toString(), searchRequestParameters).then(processData);
+    }
+
+    function loadMore(offset, limit, loadAll) {
+        lastExecutedSearchRequestParameters.offset = offset;
+        lastExecutedSearchRequestParameters.limit = limit;
+        lastExecutedSearchRequestParameters.loadAll = angular.isDefined(loadAll) ? loadAll : false;
+
+        return $http.post(lastExecutedQuery.toString(), lastExecutedSearchRequestParameters).then(processData);
+    }
+
+
+    function processData(response) {
+        var searchResults = response.data.searchResults;
+        var indexerSearchMetaDatas = response.data.indexerSearchMetaDatas;
+        var numberOfAvailableResults = response.data.numberOfAvailableResults;
+        var numberOfRejectedResults = response.data.numberOfRejectedResults;
+        var numberOfDuplicateResults = response.data.numberOfDuplicateResults;
+        var numberOfAcceptedResults = response.data.numberOfAcceptedResults;
+        var numberOfProcessedResults = response.data.numberOfProcessedResults;
+        var rejectedReasonsMap = response.data.rejectedReasonsMap;
+        var notPickedIndexersWithReason = response.data.notPickedIndexersWithReason;
+
+        lastResults = {
+            "searchResults": searchResults,
+            "indexerSearchMetaDatas": indexerSearchMetaDatas,
+            "numberOfAvailableResults": numberOfAvailableResults,
+            "numberOfAcceptedResults": numberOfAcceptedResults,
+            "numberOfRejectedResults": numberOfRejectedResults,
+            "numberOfProcessedResults": numberOfProcessedResults,
+            "numberOfDuplicateResults": numberOfDuplicateResults,
+            "rejectedReasonsMap": rejectedReasonsMap,
+            "notPickedIndexersWithReason": notPickedIndexersWithReason
+
+        };
+        // console.timeEnd("searchonly");
+        return lastResults;
+    }
+
+    function getLastResults() {
+        return lastResults;
+    }
+}
