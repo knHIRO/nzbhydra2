890190b510e2b46ae3e70071cc1612d36fb20039
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:40:24 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/springconfig/GracefulSpringShutdown.java
@@ -0,0 +1,79 @@
+/*
+ *  (C) Copyright 2017 TheOtherP (theotherp@posteo.net)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.nzbhydra.springconfig;
+
+import org.apache.catalina.connector.Connector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class GracefulSpringShutdown implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableServletWebServerFactory factory) {
+        if (factory instanceof TomcatServletWebServerFactory) {
+            ((TomcatServletWebServerFactory) factory).addConnectorCustomizers(new GracefulShutdown());
+        }
+    }
+
+    private static class GracefulShutdown implements TomcatConnectorCustomizer,
+            ApplicationListener<ContextClosedEvent> {
+
+        private static final Logger log = LoggerFactory.getLogger(GracefulShutdown.class);
+
+        private volatile Connector connector;
+
+
+        @Override
+        public void customize(Connector connector) {
+            this.connector = connector;
+        }
+
+        @Override
+        public void onApplicationEvent(ContextClosedEvent event) {
+            if (connector != null) {
+
+                this.connector.pause();
+                Executor executor = this.connector.getProtocolHandler().getExecutor();
+                if (executor instanceof ThreadPoolExecutor) {
+                    try {
+                        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
+                        threadPoolExecutor.shutdown();
+                        if (!threadPoolExecutor.awaitTermination(30, TimeUnit.SECONDS)) {
+                            log.warn("Tomcat thread pool did not shut down gracefully within "
+                                    + "30 seconds. Proceeding with forceful shutdown");
+                        }
+                    } catch (InterruptedException ex) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+        }
+
+    }
+}
