6f9d13ef890e082afe1c1b06e3326da7e91461d2
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:42:55 +0900

    update file

---
+++ core/src/test/java/org/nzbhydra/searching/searchrequests/SearchRequestTest.java
@@ -0,0 +1,30 @@
+package org.nzbhydra.searching.searchrequests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.nzbhydra.config.SearchSource;
+import org.nzbhydra.config.searching.SearchType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SearchRequestTest {
+
+    private SearchRequest testee;
+
+    @BeforeEach
+    public void setUp() {
+        testee = new SearchRequest(SearchSource.INTERNAL, SearchType.SEARCH, 0, 100);
+    }
+
+    @Test
+    void shouldFindAndRemoveExclusions() {
+        testee.setQuery("one two --three --four");
+        testee = testee.extractForbiddenWords();
+        assertThat(testee.getInternalData().getForbiddenWords()).hasSize(2);
+        assertThat(testee.getQuery().get()).isEqualTo("one two");
+        assertTrue(testee.getInternalData().getForbiddenWords().contains("three"));
+    }
+
+
+}
