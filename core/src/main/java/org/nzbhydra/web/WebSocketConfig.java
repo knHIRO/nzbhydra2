440d03384e5b6d36b717668db935d3e0b180ea08
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:40:34 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/web/WebSocketConfig.java
@@ -0,0 +1,70 @@
+/*
+ *  (C) Copyright 2021 TheOtherP (theotherp@posteo.net)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.nzbhydra.web;
+
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration(proxyBeanMethods = false)
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private ThreadPoolTaskExecutor taskExecutor;
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket")
+                .setAllowedOriginPatterns("*")
+                .withSockJS()
+                .setClientLibraryUrl("//cdn.jsdelivr.net/sockjs/1.0.3/sockjs.min.js");
+
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        this.taskExecutor = new ThreadPoolTaskExecutor();
+        this.taskExecutor.setCorePoolSize(2);
+        this.taskExecutor.setAllowCoreThreadTimeOut(true);
+        registration.taskExecutor(taskExecutor);
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        this.taskExecutor = new ThreadPoolTaskExecutor();
+        this.taskExecutor.setCorePoolSize(2);
+        this.taskExecutor.setAllowCoreThreadTimeOut(true);
+        registration.taskExecutor(taskExecutor);
+    }
+
+    @PreDestroy
+    public void onShutdown() {
+        taskExecutor.shutdown();
+    }
+
+}
