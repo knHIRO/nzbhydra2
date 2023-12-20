0d95f84d17d2b2743d0ef0cb60d92413bdeb915b
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:39:34 +0900

    update file

---
+++ core/src/main/java/org/nzbhydra/downloading/FileDownloadEvent.java
@@ -0,0 +1,35 @@
+/*
+ *  (C) Copyright 2017 TheOtherP (theotherp@posteo.net)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.nzbhydra.downloading;
+
+import lombok.Getter;
+import org.nzbhydra.searching.db.SearchResultEntity;
+
+@Getter
+public class FileDownloadEvent {
+
+
+    private final FileDownloadEntity fileDownloadEntity;
+
+    private final SearchResultEntity searchResultEntity;
+
+
+    public FileDownloadEvent(FileDownloadEntity fileDownloadEntity, SearchResultEntity searchResultEntityHasDownloaded) {
+        this.fileDownloadEntity = fileDownloadEntity;
+        this.searchResultEntity = searchResultEntityHasDownloaded;
+    }
+}
