bff6a7d4774ac4acd6e0c1d79dbd7258246269d2
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:38:03 +0900

    update file

---
+++ core/ui-src/js/directives/save-or-send-torrent.js
@@ -0,0 +1,40 @@
+angular
+    .module('nzbhydraApp')
+    .directive('saveOrSendFile', saveOrSendFile);
+
+function saveOrSendFile() {
+    return {
+        templateUrl: 'static/html/directives/save-or-send-file.html',
+        scope: {
+            searchResultId: "<",
+            isFile: "<",
+            type: "<"
+        },
+        controller: controller
+    };
+
+    function controller($scope, $http, growl, ConfigService) {
+        $scope.cssClass = "glyphicon-save-file";
+        var endpoint;
+        if ($scope.type === "TORRENT") {
+            $scope.enableButton = !_.isNullOrEmpty(ConfigService.getSafe().downloading.saveTorrentsTo) || ConfigService.getSafe().downloading.sendMagnetLinks;
+            $scope.tooltip = "Save torrent to black hole or send magnet link";
+            endpoint = "internalapi/saveOrSendTorrent";
+        } else {
+            $scope.tooltip = "Save NZB to black hole";
+            $scope.enableButton = !_.isNullOrEmpty(ConfigService.getSafe().downloading.saveNzbsTo);
+            endpoint = "internalapi/saveNzbToBlackhole";
+        }
+        $scope.add = function () {
+            $scope.cssClass = "nzb-spinning";
+            $http.put(endpoint, $scope.searchResultId).then(function (response) {
+                if (response.data.successful) {
+                    $scope.cssClass = "glyphicon-ok";
+                } else {
+                    $scope.cssClass = "glyphicon-remove";
+                    growl.error(response.data.message);
+                }
+            });
+        };
+    }
+}
