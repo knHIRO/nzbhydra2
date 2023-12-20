167c28bb33a8cf54f2b6932384644c61715f1074
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:46:53 +0900

    update file

---
+++ shared/mapping/src/main/java/org/nzbhydra/mapping/newznab/xml/caps/CapsXmlServer.java
@@ -0,0 +1,53 @@
+/*
+ *  (C) Copyright 2017 TheOtherP (theotherp@posteo.net)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.nzbhydra.mapping.newznab.xml.caps;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import lombok.Data;
+import org.nzbhydra.springnative.ReflectionMarker;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@Data
+@ReflectionMarker
+public class CapsXmlServer {
+
+    public CapsXmlServer() {
+    }
+
+    @XmlAttribute
+    private String version;
+
+    @XmlAttribute
+    private String title;
+
+    @XmlAttribute
+    private String strapline;
+
+    @XmlAttribute
+    private String email;
+
+    @XmlAttribute
+    private String url;
+
+    @XmlAttribute
+    private String image; //LATER favicon
+
+
+}
